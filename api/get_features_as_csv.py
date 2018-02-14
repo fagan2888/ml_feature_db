3909b669714c37f6e407f6474802b9ccb4b7454a
Roope Tervo <roope.tervo@fmi.fi>
2018-02-14 11:59:12 +0200

    Designed directory structure

---
+++ api/get_features_as_csv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import psycopg2
+from config import config
+from configparser import ConfigParser
+from lib import mlfb
+
+# Example script to use mlfb class
+
+
