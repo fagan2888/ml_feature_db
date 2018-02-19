472609d3199882a8ffe17fd4f5213fe3f4f22c89
Santeri Oksman <santeri.oksman@fmi.fi>
2018-02-19 14:22:47 +0200

    Added a simple test program which uses mlfb class.

---
+++ api/test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from configparser import ConfigParser
+from lib import mlfb
+
+# Example script to use mlfb class
+
+a = mlfb.mlfb(1)
+a.get_rows_trains()
+
