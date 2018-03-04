d80b51068fd5f1a531fb32422311a7c8956ea272
Roope Tervo <roope.tervo@fmi.fi>
2018-03-04 14:59:02 +0200

    Added functions to add locations and rows

--- api/lib/mlfb.py
+++ api/lib/mlfb.py
@@ -4,16 +4,23 @@ import psycopg2
 from configparser import ConfigParser
 import logging
 import numpy as np
+import os
 
 class mlfb(object):
 
     conn = None
     config_filename = None
+    schema = 'traindata'
     id = 1
     
-    def __init__(self, id=1, logging_level='INFO', config_filename='cnf/database.ini'):
+    def __init__(self, id=1, logging_level='INFO', config_filename=None, schema='traindata'):
         self.id = id
+        self.schema = schema
+
+        if config_filename is None:
+            config_filename = os.path.dirname(os.path.abspath(__file__))+'/../cnf/database.ini'
         self.config_filename = config_filename
+
 
     def config(self, section='postgresql'):
         # create a parser
@@ -108,7 +115,110 @@ class mlfb(object):
         print(header)
         print(np.array(result))
 
+    def add_point_locations(self, locations, check_for_duplicates=False):
+        """
+        Add locations to the db
+        
+        locations : list
+                    location information in following format: ['name', 'lat', 'lon']
+        """
+
+        ids = []
+        if check_for_duplicates:
+            for loc in locations:
+                id = self.get_location_by_name(loc[0])
+            
+                if id is not None:
+                    logging.debug('Found id {} for location named {}'.format(id, loc[0]))
+                    ids.append(id)
+                else:
+                    logging.debug('Location with name {} not found, creating...'.format(loc[0]))
+                    sql = "INSERT INTO {schema}.location (name, geom) VALUES ('{name}', ST_GeomFromText('POINT({lon} {lat})'))".format(name=loc[0], lat=loc[1], lon=loc[2], schema=self.schema)
+                    logging.debug(sql)
+                    self.execute(sql)
+                    id = self.get_location_by_name(loc[0])
+                    logging.debug(id)
+                    ids.append(id)
+        else:
+            sql = "INSERT INTO {schema}.location (name, geom) VALUES ".format(schema=self.schema)
+            first = True
+            for loc in locations:
+                if not first:
+                    sql = sql+', '
+                else:
+                    first = False
+                sql = sql + "('{name}', ST_GeomFromText('POINT({lon} {lat})'))".format(name=loc[0], lat=loc[1], lon=loc[2])
+            self.execute(sql)
+
+    def add_rows(self, _type, header, data, metadata, source):
+        """
+        Add rows to the db
+        
+        type     : String
+                   feature or label
+        header   : list        
+                   list containing data header (i.e. 'temperature';'windspeedms')
+        data     : np.array      
+                   numpy array or similar containing data in the same order with header
+        metadata : list
+                   list containing metadata in following order ['time', 'location_id']
+        source   : String
+                   optional source information
+        """
+
+        logging.info('Trying to insert {} {}s with source {}'.format(len(data), _type, source))
+        
+        sql = "INSERT INTO {schema}.data (type, source, time, location_id, parameter, value, row) VALUES ".format(schema=self.schema)
+        i = 0
+        first = True
+        for row in data:            
+            j = 0
+            row = source+'-'+str(i)
+            for param in header:
+                if metadata[i][1] is None:
+                    logging.error('No location for row {}'.format(i))
+                    continue
+                
+                if not first: sql = sql+', '
+                else: first = False
+                sql = sql + "('{_type}', '{source}', '{time}', {location_id}, '{parameter}', {value}, '{row}')".format(_type=_type, source=source, time=metadata[i][0].strftime('%Y-%m-%d %H:%M:%S'), location_id=metadata[i][1], parameter=param, value=data[i][j], row=row)
+                j += 1                        
+            i +=1
+
+        logging.debug(sql)
+        self.execute(sql)        
+
+    def get_locations_by_name(self, names):
+        """
+        Find location ids by names
+        
+        names : list
+                list of location names
+        """
+        sql = "SELECT id, name FROM {}.location WHERE name IN ({})".format(self.schema, '\''+'\',\''.join(names)+'\'')
+        logging.debug(sql)
+
+        return self._query(sql)
+    
+    def get_location_by_name(self, name):
+        """
+        Find location id by name
+        
+        name : String
+               location name
+        
+        return id (int) or None
+        """
+        sql = "SELECT id FROM {schema}.location WHERE name='{name}'".format(schema=self.schema, name=name)
+        logging.debug(sql)
+        res = self._query(sql)
+        if len(res) > 0:
+            return int(res[0][0])
+
+        return None
+        
     def execute(self, statement):
+
         """
         Execute single SQL statement in
         a proper manner
